c237f1c8518c654b0c7a029a52034d3320c2be3e
adac-21 <c.mmadu@alustudent.com>
2026-05-14 01:53:19 +0400

    all

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,10 +3,12 @@
 
 def safe_print_division(a, b):
     result = None
+
     try:
         result = a / b
     except ZeroDivisionError:
-        result = None
+        pass
     finally:
         print("Inside result: {}".format(result))
     return result
+print(safe_print_division(10, 2))
